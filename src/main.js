ed9d5783c8d1903368cf88d8539b44f9a3410ea2
LJJ <LiJunJie200091@163.com>
2020-01-07 17:47:44 +0800

    导入Vant 介绍 导入 封装注册Vant组件模块

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 注册 Vant 组件
+import './utils/register-vant'
 
 Vue.config.productionTip = false
 
