74aada8f09d585f7943c7dd978ec3c90ed0878b8
LJJ <LiJunJie200091@163.com>
2020-01-28 12:33:29 +0800

    布局样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,15 +2,27 @@
   <div class="login-container">
       <!-- 导航栏 -->
     <van-nav-bar title="登录"/>
+    <!-- 导航栏 -->
+
     <!-- 登录表单 -->
     <van-cell-group>
-        <van-field label="手机号" placeholder="请输入手机号"/>
-        <van-field label="验证码" placeholder="请输入验证码">
+        <van-field
+        required
+        clearable
+        label="手机号"
+        placeholder="请输入手机号"
+        />
+
+        <van-field
+        label="验证码"
+        placeholder="请输入验证码"
+        required
+        >
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
         </van-field>
     </van-cell-group>
     <!-- 登录按钮 -->
-    <div>
+    <div class="btn-wrap">
         <van-button type="info">登录</van-button>
     </div>
   </div>
@@ -23,6 +35,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.btn-wrap {
+  padding: 27px 16px;
+  .van-button {
+    width: 100%;
+    background: #6db4fb;
+  }
+}
 </style>
